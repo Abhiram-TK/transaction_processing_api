331c5dbf8b57fccf6cee3060993104e519a91e7f
Abhiram TK <abhiramtksuresh@gmail.com>
2026-05-25 16:38:34 +0530

    Implemented transaction CRUD API workflows

--- app/api/transaction_routes.py
+++ app/api/transaction_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.operations.transaction_ops import create_transaction
+from app.operations.transaction_ops import (create_transaction, get_all_transactions, get_transaction_by_id, update_transactions)
 
 router = APIRouter()
 
@@ -10,4 +10,29 @@ def create_transaction_route(customer_name: str, invoice_number: str, amount: fl
     transaction = create_transaction(customer_name, invoice_number, amount, status)
 
     return transaction
+
+@router.get("/transactions")
+def fetch_all_transactions():
+
+    transactions = get_all_transactions()
+
+    return transactions
+
+@router.get("/transactions/{transaction_id}")
+def fetch_transaction(transaction_id: int):
+
+    transaction = get_transaction_by_id(transaction_id)
+
+    return transaction
+
+@router.put("/transactions/{transaction_id}")
+def update_transaction_route(transcation_id: int, customer_name: str, amount: float, status: str):
+
+    transaction = update_transactions(transcation_id, customer_name, amount, status)
+
+    return transaction
+
+
+
+
 
--- app/operations/transaction_ops.py
+++ app/operations/transaction_ops.py
@@ -18,3 +18,57 @@ def create_transaction(customer_name, invoice_number, amount, status):
 
     return new_transaction
 
+def get_all_transactions():
+
+    db = SessionLocal()
+
+    transactions = db.query(Transaction).all()
+
+    db.close()
+
+    return transactions
+
+def get_transaction_by_id(transaction_id):
+
+    db = SessionLocal()
+
+    transaction = db.query(Transaction).filter(Transaction.id == transaction_id).first()
+
+    db.close()
+
+    return transaction
+
+def update_transactions(transaction_id, customer_name, amount, status):
+
+    db = SessionLocal()
+
+    transaction = db.query(Transaction).filter(Transaction.id == transaction_id).first()
+
+    if not transaction:
+
+        db.close()
+
+        return None
+    
+    if amount <= 0:
+
+        db.close()
+
+        return None
+    
+    transaction.customer_name = customer_name
+
+    transaction.amount = amount
+
+    transaction.status = status
+
+    db.commit()
+
+    db.refresh(transaction)
+
+    db.close()
+
+    return transaction
+
+
+
