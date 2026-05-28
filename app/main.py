66d693f2ed2682d72e1d5cc9ff6054b564cb5987
Abhiram TK <abhiramtksuresh@gmail.com>
2026-05-28 16:47:52 +0530

    Added JWT service to transaction API

--- app/main.py
+++ app/main.py
@@ -7,11 +7,15 @@ from app.models.transaction import Transaction
 
 from app.api.transaction_routes import router
 
+from app.services.jwt_service import (create_access_token, decode_access_token)
+
+
 app = FastAPI()
 
 app.include_router(router)
 
 Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 def home():
