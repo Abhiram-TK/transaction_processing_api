14ef4faec67362528d03de2362fb268e1e84dc28
Abhiram TK <abhiramtksuresh@gmail.com>
2026-05-29 17:21:06 +0530

    Added centralized configuration to transaction API

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,17 +1,10 @@
 from sqlalchemy import create_engine
 
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-from dotenv import load_dotenv
+from app.core.config import settings
 
-import os
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
--- app/services/jwt_service.py
+++ app/services/jwt_service.py
@@ -2,27 +2,18 @@ from datetime import datetime, timedelta
 
 from jose import jwt, JWTError
 
-from dotenv import load_dotenv
-
-import os
-
-
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+from app.core.config import settings
 
 
 def create_access_token(data: dict):
 
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
 
     to_encode.update({"exp": expire})
 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
     return encoded_jwt
 
@@ -30,7 +21,7 @@ def create_access_token(data: dict):
 def decode_access_token(token: str):
 
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         return payload
 
     except JWTError:
